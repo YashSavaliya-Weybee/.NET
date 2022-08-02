89d276dadaeb83b91eb3bfb3086c751b5d6cf85e
YashSavaliya-Weybee <108047426+YashSavaliya-Weybee@users.noreply.github.com>
2022-08-02 16:11:26 +0530

    hello

---
+++ Hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int a = 10, b = 5;
+            int total = a + b;
+            Console.WriteLine("Total : {0} + {1} = {2}",a,b,total);
+            Console.ReadLine();
+        }
+    }
+}
